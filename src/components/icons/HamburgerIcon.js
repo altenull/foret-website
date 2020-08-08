e3b132ad23a4b6ec06ac7d278e2d89fbd7309abd
Heonyoung Kim <altenull@gmail.com>
2020-08-09 00:03:11 +0900

    feat: implement animation of HamburgerIcon depending on isDrawerShowing

--- src/components/icons/HamburgerIcon.js
+++ src/components/icons/HamburgerIcon.js
@@ -3,29 +3,50 @@ import { css } from '@emotion/core';
 import React from 'react';
 
 const hamburgerIconStyles = css`
+  display: block;
+  width: 24px;
   height: 16px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
+  position: relative;
   cursor: pointer;
 `;
 
 const lineStyles = css`
-  min-width: 24px;
-  min-height: 1px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 2px;
+  border-radius: 1px;
   background-color: ${Color.Ink};
+  transition: top 0.3s ease-in-out, left 0.15s ease-in-out, right 0.15s ease-in-out, transform 0.3s ease-in-out;
+`;
+
+const topLineStyles = (isDrawerShowing) => css`
+  ${lineStyles};
+  top: ${isDrawerShowing ? '50%' : '0'};
+  left: ${isDrawerShowing ? '50%' : '0'};
+  right: ${isDrawerShowing ? '50%' : '0'};
+`;
+
+const middleLineStyles = (isDrawerShowing, direction) => css`
+  ${lineStyles};
+  top: 50%;
+  transform: rotate(${isDrawerShowing ? 45 * direction + 'deg' : '0'});
+`;
+
+const bottomLineStyles = (isDrawerShowing) => css`
+  ${lineStyles};
+  top: ${isDrawerShowing ? '50%' : '100%'};
+  left: ${isDrawerShowing ? '50%' : '0'};
+  right: ${isDrawerShowing ? '50%' : '0'};
 `;
 
 const HamburgerIcon = ({ isDrawerShowing }) => {
-  // TODO: Implement animation depending on isDrawerShowing value
-  //       Benchmark https://www.greshamsmith.com/
-
   return (
     <span css={hamburgerIconStyles}>
-      <span css={lineStyles} />
-      <span css={lineStyles} />
-      <span css={lineStyles} />
+      <span css={topLineStyles(isDrawerShowing)} />
+      <span css={middleLineStyles(isDrawerShowing, 1)} />
+      <span css={middleLineStyles(isDrawerShowing, -1)} />
+      <span css={bottomLineStyles(isDrawerShowing)} />
     </span>
   );
 };
