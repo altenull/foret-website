403c8fdd0ffc41fcd67ffe2b36cb5574360fd1bb
Heonyoung Kim <altenull@gmail.com>
2020-08-13 22:06:11 +0900

    refactor: apply global styles with wrapRootElement function in gatsby-browser.js not in Layout component

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,34 +1,5 @@
+import { css } from '@emotion/core';
 import React from 'react';
-import { Global, css } from '@emotion/core';
-import { Color } from '@altenull/foret-core';
-
-const globalStyles = css`
-  body {
-    margin: 0;
-    padding: 0;
-    min-height: 100vh;
-    max-width: 100vw;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
-  span,
-  div {
-    &::selection {
-      background: ${Color.ForetGreen};
-    }
-  }
-
-  #___gatsby,
-  #gatsby-focus-wrapper {
-    height: 100vh;
-  }
-`;
 
 const layoutStyles = css`
   height: 100%;
@@ -37,7 +8,6 @@ const layoutStyles = css`
 const Layout = ({ children, ...props }) => {
   return (
     <div css={layoutStyles} {...props}>
-      <Global styles={globalStyles} />
       {children}
     </div>
   );
