0340900dcc2efe8940498700a73ee6f62561f0c7
Heonyoung Kim <altenull@gmail.com>
2020-08-21 15:28:41 +0900

    feat: add content of get-started page into intl files

--- src/components/get-started/HeroSection.js
+++ src/components/get-started/HeroSection.js
@@ -2,16 +2,19 @@ import { Heading1, Subtitle1 } from '@altenull/foret-react';
 import { css } from '@emotion/core';
 import React from 'react';
 import { ResponsiveContentLayout } from '../common';
+import { useIntl } from 'gatsby-plugin-intl';
 
 const sectionStyles = css`
   padding: 24rem 0 16rem;
 `;
 
 const HeroSection = () => {
+  const intl = useIntl();
+
   return (
     <section css={sectionStyles}>
       <ResponsiveContentLayout>
-        <Heading1>Get Started</Heading1>
+        <Heading1>{intl.formatMessage({ id: 'getStarted.hero.title' })}</Heading1>
         <Subtitle1>Our components are served as an npm packages.</Subtitle1>
         <Subtitle1>Our components are served both angular and react versions</Subtitle1>
       </ResponsiveContentLayout>
