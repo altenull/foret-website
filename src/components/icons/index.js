50945cfbb31cf73f05bc7ef8594c65c2bb4480fb
Heonyoung Kim <altenull@gmail.com>
2020-08-23 15:24:39 +0900

    feat: create ArrowLeftIcon and ArrowRightIcon components

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,1 +1,3 @@
+export { default as ArrowLeftIcon } from './ArrowLeftIcon';
+export { default as ArrowRightIcon } from './ArrowRightIcon';
 export { default as HamburgerIcon } from './HamburgerIcon';
