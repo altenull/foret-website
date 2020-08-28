81f33904d1a154364462d1af9e66f8ad12f202ac
Heonyoung Kim <altenull@gmail.com>
2020-08-28 14:24:39 +0900

    feat: add design principle page

---
+++ src/pages/design-principle.js
@@ -0,0 +1,57 @@
+import { Color } from '@altenull/foret-core';
+import { css } from '@emotion/core';
+import { useIntl } from 'gatsby-plugin-intl';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Layout, PageNavigationSection } from '../components/common';
+import { useGetSiteMetadata } from '../hooks';
+
+const designPrincipleStyles = css`
+  background-color: ${Color.Paper};
+`;
+
+const DesignPrinciplePage = ({ location }) => {
+  const intl = useIntl();
+  const { siteMetadata } = useGetSiteMetadata();
+
+  const currentPageRouteIndex = siteMetadata.pageRoutes.findIndex(({ key, ...rest }) =>
+    location.pathname.includes(key)
+  );
+
+  const designPrincipleTitle = `${intl.formatMessage({
+    id: `pages.${siteMetadata.pageRoutes[currentPageRouteIndex].camelCase}`,
+  })} | ${intl.formatMessage({ id: 'title' })}`;
+
+  const getPageNavigationLinks = (currentPageRouteIndex, pageRoutes) => {
+    const prevPageNavigationIndex = currentPageRouteIndex - 1 < 0 ? pageRoutes.length - 1 : currentPageRouteIndex - 1;
+    const nextPageNavigationIndex = currentPageRouteIndex + 1 > pageRoutes.length - 1 ? 0 : currentPageRouteIndex + 1;
+
+    return {
+      prevLink: {
+        to: `/${pageRoutes[prevPageNavigationIndex].key}`,
+        text: intl.formatMessage({
+          id: `pages.${pageRoutes[prevPageNavigationIndex].camelCase}`,
+        }),
+      },
+      nextLink: {
+        to: `/${pageRoutes[nextPageNavigationIndex].key}`,
+        text: intl.formatMessage({
+          id: `pages.${pageRoutes[nextPageNavigationIndex].camelCase}`,
+        }),
+      },
+    };
+  };
+
+  const { prevLink, nextLink } = getPageNavigationLinks(currentPageRouteIndex, siteMetadata.pageRoutes);
+
+  return (
+    <Fragment>
+      <Helmet title={designPrincipleTitle} defer={false} />
+      <Layout css={designPrincipleStyles}>
+        <PageNavigationSection prevLink={prevLink} nextLink={nextLink} />
+      </Layout>
+    </Fragment>
+  );
+};
+
+export default DesignPrinciplePage;
