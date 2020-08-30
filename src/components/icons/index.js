aa2b74f266cb7739639198b323beb917b3b41722
Heonyoung Kim <altenull@gmail.com>
2020-08-30 15:50:01 +0900

    feat: create LinkIcon component

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as ArrowLeftIcon } from './ArrowLeftIcon';
 export { default as ArrowRightIcon } from './ArrowRightIcon';
 export { default as HamburgerIcon } from './HamburgerIcon';
+export { default as LinkIcon } from './LinkIcon';
