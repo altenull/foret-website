19773b2df96093c2d6984595f278e56c9890c23c
Heonyoung Kim <altenull@gmail.com>
2020-08-31 19:02:07 +0900

    style: set styles on TOC component

--- src/components/components/TOC.js
+++ src/components/components/TOC.js
@@ -1,22 +1,35 @@
-import { SmallText, Subtitle2 } from '@altenull/foret-react';
+import { Color, FontWeight } from '@altenull/foret-core';
+import { SmallText } from '@altenull/foret-react';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
 import React from 'react';
 
 const tocStyles = css`
-  display: inline-block;
   position: fixed;
   right: 160px;
-  top: 400px;
+  top: 360px;
+  padding-left: 8px;
+  border-left: 2px solid ${Color.Fog};
 `;
 
 const tocItemContainerStyles = css`
+  display: flex;
+  flex-direction: column;
   margin: 0;
   padding: 0;
 `;
 
-const linkStyles = css`
+const tocItemStyles = css`
+  display: inline-block;
   text-decoration: none;
+  & + & {
+    margin-top: 8px;
+  }
+`;
+
+const linkTextStyles = (isActivated) => css`
+  font-weight: ${isActivated ? FontWeight.semiBold : FontWeight.light};
+  color: ${isActivated ? Color.ForetGreen : Color.Stone};
 `;
 
 const TOC = ({ items, currentHash, scrollTo }) => {
@@ -24,8 +37,8 @@ const TOC = ({ items, currentHash, scrollTo }) => {
     <nav css={tocStyles}>
       <ul css={tocItemContainerStyles}>
         {items.map(({ to, text }) => (
-          <Link key={to} css={linkStyles} to={to} onClick={() => scrollTo(to)}>
-            {to === currentHash ? <Subtitle2>{text}</Subtitle2> : <SmallText>{text}</SmallText>}
+          <Link key={to} css={tocItemStyles} to={to} onClick={() => scrollTo(to)}>
+            <SmallText css={linkTextStyles(to === currentHash)}>{text}</SmallText>
           </Link>
         ))}
       </ul>
