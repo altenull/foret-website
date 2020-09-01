c984bd0015628c925dbb17e4e6b9e383a93b41e1
Heonyoung Kim <altenull@gmail.com>
2020-09-01 12:20:19 +0900

    feat: add TOC component to components page and subscribe intersection observer to update hash of url

--- src/pages/components.js
+++ src/pages/components.js
@@ -1,10 +1,12 @@
 import { Color } from '@altenull/foret-core';
 import { css } from '@emotion/core';
+import { navigate } from 'gatsby';
 import { useIntl } from 'gatsby-plugin-intl';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Layout, PageNavigationSection } from '../components/common';
-import { ButtonSection, CheckboxSection, HeroSection } from '../components/components';
+import { ButtonSection, CheckboxSection, HeroSection, TOC } from '../components/components';
+import { ComponentHashEnum } from '../enums/components/component-hash.enum';
 import { useGetSiteMetadata } from '../hooks';
 import { getPageNavigationLinks, getPageTitle } from '../utils/page.utils';
 
@@ -13,22 +15,68 @@ const componentsStyles = css`
 `;
 
 const ComponentsPage = ({ location }) => {
+  const [currentHash, setCurrentHash] = useState('');
+  const heroSectionRef = useRef();
+
   const intl = useIntl();
   const { siteMetadata } = useGetSiteMetadata();
 
-  const sectionComponentsWithHash = [
-    {
-      hash: '#button',
-      component: ButtonSection,
-    },
-    {
-      hash: '#checkbox',
-      component: CheckboxSection,
-    },
-  ];
+  const componentHashes = [ComponentHashEnum.Button, ComponentHashEnum.Checkbox];
 
-  const scrollTo = (hash) => {
-    const targetElement = document.getElementById(hash);
+  const checkHashAndScrollTo = () => {
+    if (!!location.hash) {
+      scrollTo(location.hash);
+    }
+  };
+
+  useEffect(checkHashAndScrollTo, []);
+
+  const subscribeAnchorHeadingIo = () => {
+    const anchorHeadingIoCallback = (entries) => {
+      const hasNoIntersectingEntry = !entries.find((entry) => entry.isIntersecting);
+
+      if (hasNoIntersectingEntry) {
+        // TODO: Refactor to get isHeroSectionVisible by more accurate way.
+        // TODO: Resolve issue that occured changing locale.
+        if (heroSectionRef != null && heroSectionRef.current != null) {
+          const isHeroSectionVisible =
+            document.documentElement.scrollTop < heroSectionRef.current.getBoundingClientRect().height;
+
+          if (isHeroSectionVisible) {
+            setCurrentHash('');
+          }
+        }
+      } else {
+        entries.forEach((entry) => {
+          const isNewEntryIntersecting = entry.target.id !== currentHash;
+
+          if (entry.isIntersecting && isNewEntryIntersecting) {
+            setCurrentHash(entry.target.id);
+          }
+        });
+      }
+    };
+
+    // Intersection Observer API의 사용법과 활용 방법
+    // http://blog.hyeyoonjung.com/2019/01/09/intersectionobserver-tutorial/
+    const anchorHeadingIo = new IntersectionObserver(anchorHeadingIoCallback, { threshold: 0.3 });
+    const anchorHeadingElements = componentHashes.map((componentHash) => document.getElementById(componentHash));
+
+    anchorHeadingElements.forEach((anchorHeadingElement) => {
+      anchorHeadingIo.observe(anchorHeadingElement);
+    });
+  };
+
+  useEffect(subscribeAnchorHeadingIo, []);
+
+  useEffect(() => {
+    // TODO: Find out a way to remove hash from url programatically.
+    //       First, navigate to current pathname to able to remove hash temporary.
+    navigate(currentHash === '' ? location.pathname : currentHash);
+  }, [currentHash, location.pathname]);
+
+  const scrollTo = (target) => {
+    const targetElement = document.getElementById(target);
 
     if (targetElement != null) {
       targetElement.scrollIntoView({
@@ -45,17 +93,38 @@ const ComponentsPage = ({ location }) => {
   const componentsTitle = getPageTitle(intl, currentPageRouteIndex, siteMetadata.pageRoutes);
   const { prevLink, nextLink } = getPageNavigationLinks(intl, currentPageRouteIndex, siteMetadata.pageRoutes);
 
-  const getSections = () =>
-    sectionComponentsWithHash.map(({ hash, component: SectionComponent }) => (
-      <SectionComponent key={hash} headingHash={hash} />
-    ));
+  const getSections = (componentHashes) => {
+    const componentHashToSectionMap = {
+      [ComponentHashEnum.Button]: ButtonSection,
+      [ComponentHashEnum.Checkbox]: CheckboxSection,
+    };
+
+    return componentHashes.map((componentHash) => {
+      const SectionComponent = componentHashToSectionMap[componentHash];
+
+      return <SectionComponent key={componentHash} headingHash={componentHash} />;
+    });
+  };
+
+  const getTocItems = (componentHashes) => {
+    const componentHashToIntlIdMap = {
+      [ComponentHashEnum.Button]: 'components.button.title',
+      [ComponentHashEnum.Checkbox]: 'components.checkbox.title',
+    };
+
+    return componentHashes.map((componentHash) => ({
+      to: componentHash,
+      text: intl.formatMessage({ id: componentHashToIntlIdMap[componentHash] }),
+    }));
+  };
 
   return (
     <Fragment>
       <Helmet title={componentsTitle} defer={false} />
       <Layout css={componentsStyles}>
-        <HeroSection />
-        {getSections()}
+        <HeroSection ref={heroSectionRef} />
+        {getSections(componentHashes)}
+        <TOC items={getTocItems(componentHashes)} currentHash={currentHash} scrollTo={scrollTo} />
         <PageNavigationSection prevLink={prevLink} nextLink={nextLink} />
       </Layout>
     </Fragment>
