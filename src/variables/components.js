1028de0d06d3cf50e5d9afb33a2e337465f42c5e
Heonyoung Kim <altenull@gmail.com>
2020-09-03 17:24:19 +0900

    refactor: manage propsTableColumns as variable

---
+++ src/variables/components.js
@@ -0,0 +1,8 @@
+import { PropsTableColumnEnum } from '../enums/components/props-table-column.enum';
+
+export const propsTableColumns = [
+  PropsTableColumnEnum.Type,
+  PropsTableColumnEnum.Name,
+  PropsTableColumnEnum.Default,
+  PropsTableColumnEnum.Description,
+];
