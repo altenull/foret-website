12f24e6fd4c8af2278bebfcf18f26835d8d54a04
Heonyoung Kim <altenull@gmail.com>
2020-09-08 16:22:14 +0900

    feat: create TabContentWrapper component

---
+++ src/components/common/TabContentWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { css } from '@emotion/core';
+
+const tabContentWrapperStyles = css`
+  padding: 64px 0;
+`;
+
+const TabContentWrapper = ({ children }) => {
+  return <div css={tabContentWrapperStyles}>{children}</div>;
+};
+
+export default TabContentWrapper;
