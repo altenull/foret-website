233708fe8ed3fdceea8e6139d2ffa0229ec65d68
Heonyoung Kim <altenull@gmail.com>
2020-09-19 16:36:13 +0900

    chore: finalize foret logo image

--- src/hooks/foundation/useLogoImageQuery.js
+++ src/hooks/foundation/useLogoImageQuery.js
@@ -3,7 +3,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 const useLogoImageQuery = () => {
   const { logoImage } = useStaticQuery(graphql`
     query {
-      logoImage: file(relativePath: { eq: "logo-temp.png" }) {
+      logoImage: file(relativePath: { eq: "foret-logo(160x160).png" }) {
         childImageSharp {
           fixed(width: 80, height: 80) {
             ...GatsbyImageSharpFixed
