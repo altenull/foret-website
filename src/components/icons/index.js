83cb833c58c5cbb9196a305bd52ab045b183e5bb
Heonyoung Kim <altenull@gmail.com>
2020-09-22 16:29:21 +0900

    feat: create CopyIcon component

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,5 +1,6 @@
 export { default as ArrowDownIcon } from './ArrowDownIcon';
 export { default as ArrowLeftIcon } from './ArrowLeftIcon';
 export { default as ArrowRightIcon } from './ArrowRightIcon';
+export { default as CopyIcon } from './CopyIcon';
 export { default as HamburgerIcon } from './HamburgerIcon';
 export { default as LinkIcon } from './LinkIcon';
