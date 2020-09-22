4c64cb17ded7810b0c4cd79949125d6d7a3b1154
Heonyoung Kim <altenull@gmail.com>
2020-09-22 21:48:56 +0900

    refactor: add color constant

---
+++ src/components/color/ContentSection.js
@@ -0,0 +1,21 @@
+import { MarginalHeading2 } from '@altenull/foret-react';
+import React from 'react';
+import { ColorChipContainer } from '../../containers/color';
+import { ResponsiveContentLayout } from '../foundation';
+import { COLOR_WITH_NAMES } from '../../constants/color.constant';
+
+const ContentSection = () => {
+  const getColorChips = () =>
+    COLOR_WITH_NAMES.map(({ name, color }) => <ColorChipContainer key={color} name={name} color={color} />);
+
+  return (
+    <section>
+      <ResponsiveContentLayout>
+        <MarginalHeading2>Color Overview</MarginalHeading2>
+        <div>{getColorChips()}</div>
+      </ResponsiveContentLayout>
+    </section>
+  );
+};
+
+export default ContentSection;
---
+++ src/constants/color.constant.js
@@ -0,0 +1,52 @@
+import { Color } from '@altenull/foret-core';
+
+export const COLOR_WITH_NAMES = [
+  {
+    name: 'Foret Green',
+    color: Color.ForetGreen,
+  },
+  {
+    name: 'Soil',
+    color: Color.Soil,
+  },
+  {
+    name: 'Ink',
+    color: Color.Ink,
+  },
+  {
+    name: 'Paper',
+    color: Color.Paper,
+  },
+  {
+    name: 'Stone',
+    color: Color.Stone,
+  },
+  {
+    name: 'Fog',
+    color: Color.Fog,
+  },
+  {
+    name: 'Black',
+    color: Color.Black,
+  },
+  {
+    name: 'White',
+    color: Color.White,
+  },
+  {
+    name: 'Danger',
+    color: Color.Danger,
+  },
+  {
+    name: 'Warning',
+    color: Color.Warning,
+  },
+  {
+    name: 'Success',
+    color: Color.Success,
+  },
+  {
+    name: 'Information',
+    color: Color.Information,
+  },
+];
