3443d2050b28c435b59e481637d24d83e2a0e5e2
Heonyoung Kim <altenull@gmail.com>
2020-09-23 23:40:35 +0900

    style: update HeroSection of home page styles

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -1,3 +1,4 @@
+import { Color } from '@altenull/foret-core';
 import { Heading1, PrimaryButton, Subtitle1 } from '@altenull/foret-react';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
@@ -10,10 +11,28 @@ const sectionStyles = css`
   height: 100vh;
 `;
 
-const coverPositionerStyles = css`
-  position: absolute;
-  left: 50%;
-  top: 30%;
+const twoColumnsCoverStyles = css`
+  display: flex;
+  justify-content: space-between;
+  width: calc(100% - 4rem);
+  height: calc(100% - 220px);
+  max-width: 1440px;
+  padding-top: 220px;
+  margin: 0 auto;
+`;
+
+const columnStyles = css`
+  flex: 1;
+`;
+
+const leftColumnContentPositionerStyles = css`
+  margin: 220px 0 0 96px;
+`;
+
+const dividerStyles = css`
+  flex-basis: 2px;
+  min-height: 100%;
+  background-color: ${Color.ForetGreen};
 `;
 
 const mainTitleStyles = css`
@@ -21,7 +40,7 @@ const mainTitleStyles = css`
 `;
 
 const subtitleStyles = css`
-  max-width: 592px;
+  max-width: 488px;
   margin-bottom: 40px;
 `;
 
@@ -34,12 +53,20 @@ const HeroSection = () => {
 
   return (
     <section css={sectionStyles}>
-      <div css={coverPositionerStyles}>
-        <Heading1 css={mainTitleStyles}>{intl.formatMessage({ id: 'home.hero.title' })}</Heading1>
-        <Subtitle1 css={subtitleStyles}>{intl.formatMessage({ id: 'home.hero.subtitle' })}</Subtitle1>
-        <Link to={`/${PageRouteEnum.GetStarted}`} css={linkStyles}>
-          <PrimaryButton>{intl.formatMessage({ id: 'home.hero.getStartedButton' })}</PrimaryButton>
-        </Link>
+      <div css={twoColumnsCoverStyles}>
+        <div css={columnStyles}>
+          <div css={leftColumnContentPositionerStyles}>
+            <Heading1 css={mainTitleStyles}>{intl.formatMessage({ id: 'home.hero.title' })}</Heading1>
+            <Subtitle1 css={subtitleStyles}>{intl.formatMessage({ id: 'home.hero.subtitle' })}</Subtitle1>
+            <Link to={`/${PageRouteEnum.GetStarted}`} css={linkStyles}>
+              <PrimaryButton>{intl.formatMessage({ id: 'home.hero.getStartedButton' })}</PrimaryButton>
+            </Link>
+          </div>
+        </div>
+
+        <div css={dividerStyles} />
+
+        <div css={columnStyles}></div>
       </div>
     </section>
   );
