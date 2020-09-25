7de7b3e72e80ae4291e29a266dc3a457c436acfd
Heonyoung Kim <altenull@gmail.com>
2020-09-25 22:13:43 +0900

    chore: annotate use cases of useIsMounted hook

--- src/hooks/core/useIsMounted.js
+++ src/hooks/core/useIsMounted.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 
+// Use cases of useIsMounted
+// - Determine component is mounted or not to access window or document.
+// - When component is mounted and you want to start the animation.
 const useIsMounted = () => {
   const [isMounted, setIsMounted] = useState(false);
 
