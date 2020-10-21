9087cc15ff44fda1b10b1d0bd7f01b09fd81fb8a
Heonyoung Kim <altenull@gmail.com>
2020-10-21 18:22:54 +0900

    feat: create Portal component to re-use

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,6 +2,7 @@ export { default as BaseHeroSection } from './BaseHeroSection';
 export { default as CopyIconWithCopiedMessage } from './CopyIconWithCopiedMessage';
 export { default as NextPageLink } from './NextPageLink';
 export { default as PageNavigationSection } from './PageNavigationSection';
+export { default as Portal } from './Portal';
 export { default as PrevPageLink } from './PrevPageLink';
 export { default as ScrollDownIndicator } from './ScrollDownIndicator';
 export { default as TabContentWrapper } from './TabContentWrapper';
