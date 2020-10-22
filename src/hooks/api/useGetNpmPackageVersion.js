a60227d845bc4fd54017499c7012bd41558153d4
Heonyoung Kim <altenull@gmail.com>
2020-10-22 17:52:01 +0900

    fix: request get npm package version with cors proxy regardless of node env

--- src/hooks/api/useGetNpmPackageVersion.js
+++ src/hooks/api/useGetNpmPackageVersion.js
@@ -1,18 +1,14 @@
 import { useEffect, useState } from 'react';
-import useSiteMetadataQuery from '../core/useSiteMetadataQuery';
-import { NodeEnvEnum } from '../../enums/core/node-env.enum';
 
 export const useGetNpmPackageVersion = (packageName) => {
   const [version, setVersion] = useState(null);
-
-  const { siteMetadata } = useSiteMetadataQuery();
 
   // CORS issue of npm registry api issue when requesting from localhost.
   // https://stackoverflow.com/questions/58598264/fetching-npm-registry-with-angular-httpclient-causing-cors
   const corsProxy = 'https://cors-anywhere.herokuapp.com/';
   const npmRegistryUrl = `https://registry.npmjs.org/${encodeURIComponent(packageName)}`;
 
-  const requestUrl = `${siteMetadata.nodeEnv === NodeEnvEnum.Development ? corsProxy : ''}${npmRegistryUrl}`;
+  const requestUrl = `${corsProxy}${npmRegistryUrl}`;
 
   useEffect(() => {
     fetch(requestUrl)
