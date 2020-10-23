36295d122550a3083db219df864367f8c68b8851
Heonyoung Kim <altenull@gmail.com>
2020-10-23 14:30:46 +0900

    feat: set og metatags, add foret-og.png image file

--- src/components/seo/CustomHelmet.js
+++ src/components/seo/CustomHelmet.js
@@ -1,14 +1,33 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import { useIntl } from 'gatsby-plugin-intl';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
 const CustomHelmet = ({ title, language }) => {
   const intl = useIntl();
+  const { foretOg } = useStaticQuery(graphql`
+    query {
+      foretOg: file(relativePath: { eq: "foret-og.png" }) {
+        childImageSharp {
+          fluid {
+            src
+          }
+        }
+      }
+    }
+  `);
 
   return (
     <Helmet htmlAttributes={{ lang: language }} defer={false}>
       <title>{title}</title>
       <meta name='description' content={intl.formatMessage({ id: 'description' })} />
+
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://foretdesign.io/' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={intl.formatMessage({ id: 'description' })} />
+      <meta property='og:image' content={foretOg.childImageSharp.fluid.src} />
+
       <link rel='canonical' href='https://foretdesign.io/' />
     </Helmet>
   );
